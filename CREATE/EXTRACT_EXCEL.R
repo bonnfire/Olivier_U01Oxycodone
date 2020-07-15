74dbf74183227de74545233293f65c2521c9e27b
Bonnie Lin <b2lin@ucsd.edu>
2020-07-15 15:02:40 -0700

    extract mapping files from dropbox

--- CREATE/EXTRACT_EXCEL.R
+++ CREATE/EXTRACT_EXCEL.R
@@ -7,7 +7,9 @@ cohortfiles <- list.files(pattern = "*.xlsx")
 olivieroxy_excel <- list("C01"=selfadmin_rewards_cohort1,
                          "C03"=selfadmin_rewards_cohort3,
                          "C04"=selfadmin_rewards_cohort4,
-                         "C05"=selfadmin_rewards_cohort5) %>% rbindlist(idcol = "cohort", fill = T) %>% 
+                         "C05"=selfadmin_rewards_cohort5,
+                         "C06"=selfadmin_rewards_cohort6,
+                         "C07"=selfadmin_rewards_cohort7) %>% rbindlist(idcol = "cohort", fill = T) %>% 
   clean_names() %>% # use this fxn to return df, use make_clean_names on vector 
   rename("labanimalid" = "rat") %>% 
   mutate_all(as.character)
@@ -78,7 +80,7 @@ selfadmin_rewards_cohort1 <- selfadmin_df %>% dplyr::filter(measurement == "ACTI
   
 ########################
 # COHORT 3
-########################
+########################  
 
 # this file doesn't have rfid's so will need to extract from the replacement table
 filename <- cohortfiles[2]
@@ -227,14 +229,92 @@ nm <- names(selfadmin_rewards_cohort5)[-c(1:2)] # make date columns for this vec
 nm1 <- paste("date", nm, sep = "_") # make these date columns
 selfadmin_rewards_cohort5[ , ( nm1 ) := lapply( dates, function(x) rep(x, each = .N) ) ] # make the date columns 
 
+########################
+# COHORT 6
+########################
+
+filename <- cohortfiles[5]
+selfadmin <- u01.importxlsx(filename)[[1]] %>%
+  as.data.table
+selfadmin[ selfadmin == "n/a" ] <- NA # change all character n/a to actual NA
+
+# set correct column names 
+# create date columns
+dates <- grep("^\\d+", names(selfadmin), value = T) # use these columns to make date columns # ignore the ...\\d columns
+dates <- as.character(as.POSIXct(as.numeric(dates) * (60*60*24), origin="1899-12-30", tz="UTC", format="%Y-%m-%d")) # convert Excel character into dates
+
+setnames(selfadmin, toupper(as.character(selfadmin[1,]) )) # now that dates are moved into separate vector, remove from the column names 
+setnames(selfadmin,  mgsub::mgsub(names(selfadmin), c("PR2", "^T.+1$", "^T.+2$", "^T.+3$", "^T.+4$", "SHA.*?([0-9]{2})$", "LGA.*?([0-9]{2})$"), c("PR02", "PR03_T01", "PR04_T02", "PR05_T03", "PR06_T04", "SHA\\1", "LGA\\1")))
+names(selfadmin)[1:2] <- c("RAT", "RFID")
+selfadmin <- selfadmin[-1,]
+selfadmin <- remove_empty(selfadmin, "cols") # janitor::remove_empty_cols() deprecated
+# selfadmin <- selfadmin[1:grep("average", selfadmin$RAT)[1],] # subset only the rewards table
+
+nm <- names(selfadmin)[-c(1:2)] # make date columns for this vector of exp names  ## MISSING THE RFID COLUMN SO THAT IS WHY [-1] INSTEAD OF [-c(1:2)] 
+nm1 <- paste("date", nm, sep = "_") # make these date columns
+selfadmin[ , ( nm1 ) := lapply( dates, function(x) rep(x, each = .N) ) ] # make the date columns 
+
+#extract comments
+comments_df <- selfadmin[which(selfadmin$RAT %in% c("COMMENT", "CONFLICT", "RESOLUTION"))]
+comments_df <- comments_df %>% select(-matches("RFID|date")) %>% t() %>% 
+  as.data.frame() %>% 
+  rownames_to_column()
+setnames(comments_df, append("EXP", comments_df[1, 2:3] %>% t() %>% unlist() %>% as.character) %>% tolower)
+comments_df <- comments_df[-1,]
+# selfadmin <- selfadmin[!which(selfadmin$RAT %in% c("COMMENT", "CONFLICT", "RESOLUTION"))]
+
+selfadmin_exps <- grep("REWARDS|ACTIVE|INACTIVE|PR$", selfadmin$RAT)
+selfadmin_split <- split(selfadmin, cumsum(1:nrow(selfadmin) %in% selfadmin_exps))
+names(selfadmin_split) <- lapply(selfadmin_split, function(x){ x$RAT %>% head(1)}) %>% unlist() %>% as.character()
+selfadmin_split <- lapply(selfadmin_split, function(x){ x %>% dplyr::filter(grepl("^[MF]\\d+", RAT))})
+selfadmin_df <- selfadmin_split %>% rbindlist(idcol = "measurement") %>% dplyr::filter(measurement != "COMMENT")
+selfadmin_rewards_cohort6 <- selfadmin_df %>% dplyr::filter(measurement == "REWARDS")
 
 
+########################
+# COHORT 7
+########################
+
+filename <- cohortfiles[6]
+selfadmin <- u01.importxlsx(filename)[[1]] %>%
+  as.data.table
+selfadmin[ selfadmin == "n/a" ] <- NA # change all character n/a to actual NA
+
+# set correct column names 
+# create date columns
+dates <- grep("^\\d+", names(selfadmin), value = T) # use these columns to make date columns # ignore the ...\\d columns
+dates <- as.character(as.POSIXct(as.numeric(dates) * (60*60*24), origin="1899-12-30", tz="UTC", format="%Y-%m-%d")) # convert Excel character into dates
+
+setnames(selfadmin, toupper(as.character(selfadmin[1,]) )) # now that dates are moved into separate vector, remove from the column names 
+setnames(selfadmin,  mgsub::mgsub(names(selfadmin), c("PR2", "^T.+1$", "^T.+2$", "^T.+3$", "^T.+4$", "SHA.*?([0-9]{2})$", "LGA.*?([0-9]{2})$"), c("PR02", "PR03_T01", "PR04_T02", "PR05_T03", "PR06_T04", "SHA\\1", "LGA\\1")))
+names(selfadmin)[1:2] <- c("RAT", "RFID")
+selfadmin <- selfadmin[-1,]
+selfadmin <- remove_empty(selfadmin, "cols") # janitor::remove_empty_cols() deprecated
+# selfadmin <- selfadmin[1:grep("average", selfadmin$RAT)[1],] # subset only the rewards table
+
+nm <- names(selfadmin)[-c(1:2)] # make date columns for this vector of exp names  ## MISSING THE RFID COLUMN SO THAT IS WHY [-1] INSTEAD OF [-c(1:2)] 
+nm1 <- paste("date", nm, sep = "_") # make these date columns
+selfadmin[ , ( nm1 ) := lapply( dates, function(x) rep(x, each = .N) ) ] # make the date columns 
+
+#extract comments
+comments_df <- selfadmin[which(selfadmin$RAT %in% c("COMMENT", "CONFLICT", "RESOLUTION"))]
+comments_df <- comments_df %>% select(-matches("RFID|date")) %>% t() %>% 
+  as.data.frame() %>% 
+  rownames_to_column()
+setnames(comments_df, append("EXP", comments_df[1, 2:3] %>% t() %>% unlist() %>% as.character) %>% tolower)
+comments_df <- comments_df[-1,]
+# selfadmin <- selfadmin[!which(selfadmin$RAT %in% c("COMMENT", "CONFLICT", "RESOLUTION"))]
+
+selfadmin_exps <- grep("REWARDS|ACTIVE|INACTIVE|PR$", selfadmin$RAT)
+selfadmin_split <- split(selfadmin, cumsum(1:nrow(selfadmin) %in% selfadmin_exps))
+names(selfadmin_split) <- lapply(selfadmin_split, function(x){ x$RAT %>% head(1)}) %>% unlist() %>% as.character()
+selfadmin_split <- lapply(selfadmin_split, function(x){ x %>% dplyr::filter(grepl("^[MF]\\d+", RAT))})
+selfadmin_df <- selfadmin_split %>% rbindlist(idcol = "measurement") %>% dplyr::filter(measurement != "COMMENT")
+selfadmin_rewards_cohort7 <- selfadmin_df %>% dplyr::filter(measurement == "REWARDS")
 
 
 ##################################################
 
-
-########### EXTRACT THE COMPUTER NOTES ##########
 
 ### EXTRACT THE COMPUTER NOTES FROM THEIR DROPBOX
 setwd("~/Dropbox (Palmer Lab)/GWAS (1)")
@@ -244,4 +324,29 @@ computernotes_oxy <- u01.importxlsx("computer notes.xlsx")[[2]] %>%
   dplyr::filter(grepl("^C", cohort)) %>% 
   naniar::replace_with_na(replace = list(computernote = "NA"))
 
+### EXTRACT THE MAPPING FILES FROM THEIR DROPBOX
+setwd("~/Dropbox (Palmer Lab)/Olivier_George_U01/Rat Information/Oxycodone")
+rat_info_xl_filenames <- list.files(pattern = "*.xlsx")
 
+rat_info_allcohort_xl_df <- lapply(rat_info_xl_filenames, function(x){
+  path_sheetnames <- excel_sheets(x)
+  df <- lapply(excel_sheets(path = x), read_excel, path = x) # including this, just in case the info file ever moves out of order
+  names(df) <- path_sheetnames
+  info_name <- grep("info|timeline", path_sheetnames, ignore.case = T, value = T) # allows for small changes, like info sheet vs info sheets
+  df_info <- df[[info_name]]
+  
+  df_info <- df_info %>% mutate_all(as.character) # prevent any mismatched class attributes
+  df_info <- df_info %>%
+    mutate(naive = NA, 
+           naive = replace(naive, is.na(RAT), "Empty"),
+           naive = replace(naive, grepl("Naive", RAT, ignore.case=F), "Naive")) %>% 
+    tidyr::fill(naive) %>% 
+    mutate(naive = replace(naive, !grepl("Naive", naive), NA)) %>% 
+    subset(grepl("^\\d", RFID)) # tackle the naive cases
+  
+  return(df_info)
+})
+names(rat_info_allcohort_xl_df) <- rat_info_xl_filenames
+rat_info_allcohort_xl_df %<>% rbindlist(fill = T, idcol = "cohort") %<>% 
+  mutate(cohort = str_extract(cohort, "C\\d{2}")) %<>%
+  clean_names()
